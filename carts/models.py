5cfa21b466b3b58f6cf5b77514e1c0b50f286538
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2024-03-20 17:28:03 +0600

    Create models and migrations

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from azshop.models import BaseModel
+from .enums import CartStatus
+
+
+class Cart(BaseModel):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    status = models.CharField(choices=CartStatus.choices, max_length=16, default=CartStatus.ACTIVE)
+
+    class Meta:
+        db_table = "carts"
+
+
+class CartItem(BaseModel):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        db_table = "cart_items"
