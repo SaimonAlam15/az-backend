8a53ad33630232847c4816f404a65c012ab86907
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2024-04-01 17:49:49 +0600

    Add enums, model and migrations

---
+++ orders/enums.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class OrderStatus(models.TextChoices):
+    PENDING = "pending"
+    PROCESSING = "processing"
+    COMPLETED = "completed"
+    CANCELLED = "cancelled"
+    REFUNDED = "refunded"
+    FAILED = "failed"
+    ON_HOLD = "on_hold"
+    SHIPPED = "shipped"
+    DELIVERED = "delivered"
+    RETURNED = "returned"
+    EXCHANGED = "exchanged"
+
+
+class PaymentMethod(models.TextChoices):
+    COD = "cod"
+    CARD = "card"
+    PAYPAL = "paypal"
+    OTHER = "other"
+
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from azshop.models import BaseModel
+from carts.models import Cart
+from .enums import OrderStatus, PaymentMethod
+
+
+class Order(BaseModel):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    payment_method = models.CharField(
+        choices=PaymentMethod.choices, max_length=255,
+        default=PaymentMethod.COD
+    )
+    status = models.CharField(
+        choices=OrderStatus.choices, max_length=255,
+        default=OrderStatus.PENDING
+    )
+
+    class Meta:
+        db_table = "orders"
