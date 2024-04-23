0ed25b85299bd706c1dc16d273a56e5e3a52f54d
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2024-04-23 15:12:59 +0600

    Add password reset views and templates

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import (
+    PasswordResetView, PasswordResetDoneView,
+    PasswordResetConfirmView, PasswordResetCompleteView
+)
+
 from .views import (
     RegistrationView, ActivationView, LoginView, LogoutView, UserListView,
     UserDetailView, AddressBookListView, AddressBookDetailView
@@ -11,6 +16,22 @@ urlpatterns = [
     path('signup', RegistrationView.as_view(), name='registration'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
+    path(
+        'password-reset/', PasswordResetView.as_view(
+            template_name='password_reset.html'), name='password_reset'
+    ),
+    path(
+        'password-reset-done', PasswordResetDoneView.as_view(
+            template_name='password_reset_done.html'), name='password_reset_done'
+    ),
+    path(
+        'password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html'), name='password_reset_confirm'
+    ),
+    path(
+        'password-reset-complete', PasswordResetCompleteView.as_view(
+            template_name='password_reset_complete.html'), name='password_reset_complete'
+    ),
     path(
         '<int:user_id>/address-book/', AddressBookListView.as_view(),
         name='address_book_list'
